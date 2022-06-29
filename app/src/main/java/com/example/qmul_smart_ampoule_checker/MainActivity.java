2f6eaaa40183e1bef3cb535d762395a9e7c8f804
Alejandro <alejandro.alayon92@gmail.com>
2022-06-29 14:45:10 +0100

    Add first page

--- app/src/main/java/com/example/qmul_smart_ampoule_checker/MainActivity.java
+++ app/src/main/java/com/example/qmul_smart_ampoule_checker/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.qmul_smart_ampoule_checker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private Button startButton, settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.Start_button);
+        settingsButton = findViewById(R.id.Settings_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isCameraPermissionGranted()) {
+                    startCameraActivity();
+                }
+                else {
+                    getCameraPermission();
+                }
+            }
+        });
+    }
+
+    private boolean isCameraPermissionGranted() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void startCameraActivity() {
+        Intent startIntent = new Intent(MainActivity.this, CameraActivity.class);
+        startActivity(startIntent);
+    }
+
+    private void getCameraPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCameraActivity();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "The application has no right to access the camera!. Please verify.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
