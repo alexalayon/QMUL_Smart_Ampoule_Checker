3420c922aebd41f0f3c354ae7a79a6253125321f
Alejandro <alejandro.alayon92@gmail.com>
2022-06-29 13:59:58 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QMUL_Smart_Ampoule_Checker
---
+++ app/src/main/java/com/example/qmul_smart_ampoule_checker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qmul_smart_ampoule_checker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
